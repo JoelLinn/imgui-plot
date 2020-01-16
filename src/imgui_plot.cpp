98159456e211d2ca841f9e98016d7fb228988cc8
Joel Linn <jl@conductive.de>
2020-01-16 16:16:32 +0100

    Fix selection on log scale.

--- src/imgui_plot.cpp
+++ src/imgui_plot.cpp
@@ -246,9 +246,9 @@ PlotStatus Plot(const char* label, const PlotConfig& conf) {
             }
             float fSelectionStep = 1.0 / item_count;
             ImVec2 pos0 = ImLerp(inner_bb.Min, inner_bb.Max,
-                                 ImVec2(fSelectionStep * *conf.selection.start, 0.f));
+                ImVec2(rescale(fSelectionStep * *conf.selection.start, x_min, x_max, conf.scale.type), 0.f));
             ImVec2 pos1 = ImLerp(inner_bb.Min, inner_bb.Max,
-                                ImVec2(fSelectionStep * (*conf.selection.start + *conf.selection.length), 1.f));
+                ImVec2(rescale(fSelectionStep * (*conf.selection.start + *conf.selection.length), x_min, x_max, conf.scale.type), 1.f));
             window->DrawList->AddRectFilled(pos0, pos1, IM_COL32(128, 128, 128, 32));
             window->DrawList->AddRect(pos0, pos1, IM_COL32(128, 128, 128, 128));
         }
